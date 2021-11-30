6ad66397cad2590594ae91a2762f2d22ee913d45
BENTOUIL Afaf <afaf.bentouil@gmail.com>
2021-11-30 19:12:15 +0100

    accueil

--- gestion-fiches-techniques/src/app/app.module.ts
+++ gestion-fiches-techniques/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule
