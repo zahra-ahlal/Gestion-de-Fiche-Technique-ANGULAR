385664916652c0e67d7a3500abdc7b22c8937cd5
BENTOUIL Afaf <afaf.bentouil@gmail.com>
2021-11-30 19:53:52 +0100

    not-found

--- gestion-fiches-techniques/src/app/app-routing.module.ts
+++ gestion-fiches-techniques/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AccueilComponent } from './accueil/accueil.component';
-//import { NotFoundComponent } from './not-found/not-found.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
   { path: 'accueil', component: AccueilComponent, },
-  //{ path: '**', component: NotFoundComponent }
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
