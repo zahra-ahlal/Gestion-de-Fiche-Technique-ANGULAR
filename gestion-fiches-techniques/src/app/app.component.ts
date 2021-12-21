e0950b9cbb0051677a7c71fa0ac7e7d5b9b9fdd0
zahra-ahlal <zahraahlal1997@gmail.com>
2021-12-21 01:53:20 +0100

    Login Backend - Firebase Authentication

--- gestion-fiches-techniques/src/app/app.component.ts
+++ gestion-fiches-techniques/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'gestion-fiches-techniques';
+
+  constructor(){
+    // Import the functions you need from the SDKs you need
+    // Your web app's Firebase configuration
+    
+    const firebaseConfig = {
+      apiKey: "AIzaSyB5hP4r6e4GfuFRPcTY3EIBk26sqndbaP8",
+      authDomain: "gestion-fiches-techniques.firebaseapp.com",
+      projectId: "gestion-fiches-techniques",
+      storageBucket: "gestion-fiches-techniques.appspot.com",
+      messagingSenderId: "635487850988",
+      appId: "1:635487850988:web:0a7ca2363fb6a750e330c9"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    
+  }
+  
 }
--- gestion-fiches-techniques/src/app/app.module.ts
+++ gestion-fiches-techniques/src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import {APP_BASE_HREF} from '@angular/common';
+
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
@@ -10,9 +22,15 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService, AuthGuardService,{provide: APP_BASE_HREF, useValue : '/' }],
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
