5dcffbec9038129b009a83218e20352f02bc48b8
zahra-ahlal <zahraahlal1997@gmail.com>
2021-12-26 12:43:56 +0100

    Test pour afficher la liste d'ingredients en fonction de la categorie

--- gestion-fiches-techniques/src/app/components/liste-ingredients/liste-ingredients.component.ts
+++ gestion-fiches-techniques/src/app/components/liste-ingredients/liste-ingredients.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { EditIngredientComponent } from 'src/app/modal/edit-ingredient/edit-ingredient.component';
+import { CategoriesIngredientService } from 'src/app/services/categories-ingredient.service';
 import { IngredientService } from 'src/app/services/ingredient.service';
+import { CategorieIngredientInterface } from 'src/models/categIngr.model';
 import { IngredientInterface } from 'src/models/ingredient.model';
 
 @Component({
@@ -11,13 +14,27 @@ import { IngredientInterface } from 'src/models/ingredient.model';
 })
 export class ListeIngredientsComponent implements OnInit {
 
+  idCategIngr : Number = 0;
   listeIngredients : IngredientInterface[] = [];
-  constructor(private ingrService: IngredientService, private modal: NgbModal) { }
+  constructor(private ingrService: IngredientService,private categIngrService: CategoriesIngredientService, private modal: NgbModal,private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.ingrService.getIngredients().subscribe((res: IngredientInterface[]) => {
+
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.idCategIngr = params['idCategIngr'].split(':')[1];
+          console.log(this.idCategIngr);
+        }
+    );
+    //recuperer tous les ingredients se trouvant dans le tableaux des ingredients
+    this.categIngrService.getIngredientsById(this.idCategIngr).subscribe((res: CategorieIngredientInterface) => {
+      console.log ("--->Id Ingredient " + res.listIngr);
+    });
+
+    /*this.ingrService.getIngredients().subscribe((res: IngredientInterface[]) => {
       this.listeIngredients = res;
-    })
+    })*/
   }
 
   editModal(ingredient: IngredientInterface) {
