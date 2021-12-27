23ebc319199eca8ffa56ec7fab4f5a31f9484fdc
zahra-ahlal <zahraahlal1997@gmail.com>
2021-12-27 01:28:13 +0100

    Filtrage de la liste des ingredients par l'id de la categorie avec AngularFireStore

--- gestion-fiches-techniques/src/app/components/liste-ingredients/liste-ingredients.component.ts
+++ gestion-fiches-techniques/src/app/components/liste-ingredients/liste-ingredients.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ActivatedRoute, Params } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { map } from 'rxjs/operators';
 import { EditIngredientComponent } from 'src/app/modal/edit-ingredient/edit-ingredient.component';
 import { CategoriesIngredientService } from 'src/app/services/categories-ingredient.service';
 import { IngredientService } from 'src/app/services/ingredient.service';
+import { TestIngredientsService } from 'src/app/services/test-ingredients.service';
 import { CategorieIngredientInterface } from 'src/models/categIngr.model';
 import { IngredientInterface } from 'src/models/ingredient.model';
 
@@ -14,29 +17,31 @@ import { IngredientInterface } from 'src/models/ingredient.model';
 })
 export class ListeIngredientsComponent implements OnInit {
 
-  idCategIngr : Number = 0;
-  listeIngredients : IngredientInterface[] = [];
-  constructor(private ingrService: IngredientService,private categIngrService: CategoriesIngredientService, private modal: NgbModal,private route: ActivatedRoute) { }
-
-  ngOnInit() {
-
+  idCategIngr : string = "";
+  listeIngredients : any;
+  constructor(private ingrService: IngredientService,public afAuth: AngularFireAuth,private testService: TestIngredientsService, private modal: NgbModal, private route: ActivatedRoute) { }
+  
+  ngOnInit(): void {
     this.route.params
       .subscribe(
         (params: Params) => {
           this.idCategIngr = params['idCategIngr'].split(':')[1];
-          console.log(this.idCategIngr);
-        }
-    );
-    //recuperer tous les ingredients se trouvant dans le tableaux des ingredients
-    this.categIngrService.getIngredientsById(this.idCategIngr).subscribe((res: CategorieIngredientInterface) => {
-      console.log ("--->Id Ingredient " + res.listIngr);
-    });
-
-    /*this.ingrService.getIngredients().subscribe((res: IngredientInterface[]) => {
-      this.listeIngredients = res;
-    })*/
+        });
+    this.getIngredients();
   }
 
+  getIngredients() : void {
+    this.ingrService.getByIdCateg(this.idCategIngr).snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+        )
+      )
+    ).subscribe(data => {
+      this.listeIngredients = data;
+    });
+  }
+  
   editModal(ingredient: IngredientInterface) {
     const modalRef = this.modal.open(EditIngredientComponent, {
       size: 'lg',
