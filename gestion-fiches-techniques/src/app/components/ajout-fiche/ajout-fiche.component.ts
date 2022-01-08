98d59c8a774f3b4c5b93dc73feee894a0fb65e3d
BENTOUIL Afaf <afaf.bentouil@gmail.com>
2022-01-08 13:59:16 +0100

    test affichage ingredients

--- gestion-fiches-techniques/src/app/components/ajout-fiche/ajout-fiche.component.ts
+++ gestion-fiches-techniques/src/app/components/ajout-fiche/ajout-fiche.component.ts
@@ -100,6 +100,7 @@ export class AjoutFicheComponent implements OnInit {
       )
     ).subscribe(data => {
       this.listeIngredients = data;
+      console.log(this.listeIngredients)
     });
   }
 
